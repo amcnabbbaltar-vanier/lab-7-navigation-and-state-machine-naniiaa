ca8c70672a2e720d5c01198ef4dbd774a5ce0294
Bridjette Nania Centro <bridjette_centro@yahoo.com>
2025-03-26 13:53:21 -0400

    Commit 1

--- Assets/Scripts/State/IdleState.cs
+++ Assets/Scripts/State/IdleState.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class IdleState : IState
 {
-   
+    private AIController aiController;
+    private float idleDuration = 2f;
+    private float idleTimer;
+
+    public StateType Type => StateType.Idle;
+
+    public IdleState(AIController aiController)
+    {
+        this.aiController = aiController;
+    }
+    
+    public void Enter()
+    {
+        ideTimer = 0f;
+        // aiController.Animator.SetBool("isMoving", false);
+    }
+
+    public void Execute()
+    {
+        ideTimer += Time.deltaTime;
+        
+        if (idleTimer > ideDuration) 
+        {
+            aiController.StateMachine.TransitionToState(StateType.Patrol);
+        }
+    }
+
+    public void Exit()
+    {
+        // Clean up if necessary
+    }
+
 }
 
